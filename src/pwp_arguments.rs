51edf700ba3e50f4cf0fca34054d0145591108ba
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-10-31 15:06:49 -0400

    Enhance documentation for command-line arguments
    Refine the descriptions of command-line arguments in `PWPArgs` struct to improve clarity. This includes specifying the purpose of each argument, updating the doc comments, and making terminology consistent.

--- src/pwp_arguments.rs
+++ src/pwp_arguments.rs
@@ -3,26 +3,43 @@ use clap::Parser;
 #[derive(Debug, Parser)]
 #[command(
 	version,
-	about = "A cli for automating the publishing process for actix-web projects to the mardens ubuntu server.",
+	about = "A CLI for automating the publishing process for Actix-web projects to the Mardens Ubuntu server.",
 	name = "Publish Web Project"
 )]
 pub struct PWPArgs {
+	/// Hostname or IP address of the server
 	#[arg(short, long)]
 	pub host: String,
+
+	/// Username for SSH authentication
 	#[arg(short, long)]
 	pub username: String,
+
+	/// Port number for SSH connection
 	#[arg(short, long)]
 	pub port: u16,
+
+	/// Path to the authentication file (e.g., private key)
 	#[arg(short, long = "auth")]
 	pub auth_file: Option<String>,
+
+	/// Password for SSH authentication (conflicts with auth_file)
 	#[arg(short, long, conflicts_with = "auth")]
 	pub password: Option<String>,
+
+	/// Name of the binary to be published
 	#[arg(short, long = "binary")]
 	pub binary_name: Option<String>,
+
+	/// Name of the systemd service to be managed
 	#[arg(short, long = "service")]
 	pub service_name: Option<String>,
+
+	/// Flag indicating whether to build before publishing
 	#[arg(short, long)]
 	pub build: bool,
+
+	/// Command to build the project (used if 'build' is true)
 	#[arg(short, long, default_value = "cargo build --release", requires = "build")]
 	pub build_command: String,
 }
