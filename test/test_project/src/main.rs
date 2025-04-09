75fb180a842a9e23cf96cd97429ad4de23cf4ca5
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-04-09 15:52:23 -0400

    Add test_project with a basic infinite loop implementation
    This commit initializes a new Rust project named `test_project`. It includes a simple `main.rs` file with an infinite loop demonstrating a basic structure. Metadata files and build outputs were also generated by Cargo during the project setup.

---
+++ test/test_project/src/main.rs
@@ -0,0 +1,8 @@
+use std::thread::sleep;
+use std::time::Duration;
+
+fn main() {
+    loop {
+        sleep(Duration::from_secs(1));
+    }
+}
---
+++ test/test_project/target/debug/deps/test_project-f4458cfd8960e010.d
@@ -0,0 +1,9 @@
+C:\Workspaces\Rust\publish-web-project\test\test_project\target\debug\deps\libtest_project-f4458cfd8960e010.rmeta: src\main.rs Cargo.toml
+
+C:\Workspaces\Rust\publish-web-project\test\test_project\target\debug\deps\test_project-f4458cfd8960e010.d: src\main.rs Cargo.toml
+
+src\main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
