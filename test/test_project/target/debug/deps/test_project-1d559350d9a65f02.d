42ffcf91b526949352ced00d716a06d62981c577
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-05-06 15:36:21 -0400

    Remove generated build artifacts from version control.
    The deleted files are auto-generated by the Rust build process and should not be tracked in version control. This cleanup reduces unnecessary clutter and ensures autogenerated files are not mistakenly committed in the future.

--- test/test_project/target/debug/deps/test_project-1d559350d9a65f02.d
+++
@@ -1,9 +0,0 @@
-C:\Workspaces\Rust\publish-web-project\test\test_project\target\debug\deps\libtest_project-1d559350d9a65f02.rmeta: src\main.rs Cargo.toml
-
-C:\Workspaces\Rust\publish-web-project\test\test_project\target\debug\deps\test_project-1d559350d9a65f02.d: src\main.rs Cargo.toml
-
-src\main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
